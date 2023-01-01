2615f0deabb3fc2ef109a9de96ae4fe1d69747b5
Simone <simone.giacomelli@gmail.com>
2023-01-01 11:23:11 +0000

    simplified javascript

--- pynanto/pynanto/bootstrap_pyodide.js
+++ pynanto/pynanto/bootstrap_pyodide.js
@@ -1,27 +1,6 @@
-if (typeof pyodide_src_url === 'undefined')
-    pyodide_src_url = 'https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js'
-
-async function main() {
-    if (typeof loadPyodide === 'undefined') {
-        async function load_script(src) {
-            let script = document.createElement('script');
-            script.src = src;
-            let resolve = '';
-            let promise = new Promise(r => resolve = r);
-            script.onload = () => resolve();
-            document.body.append(script)
-            await promise;
-        }
-
-        await load_script(pyodide_src_url);
-    }
-    let pyodide = await loadPyodide();
-    // await pyodide.loadPackage('micropip')
-    // const micropip = pyodide.pyimport("micropip");
-    // await micropip.install(['pydantic']);
-    pyodide.runPythonAsync(`
-# python replace marker
-  `);
+if (typeof loadPyodide === 'undefined') {
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js';
+    script.onload = async () => (await loadPyodide()).runPythonAsync(`# python replace marker`);
+    document.body.append(script)
 }
-
-main();
