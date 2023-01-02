4c4634c486bc8ee0a4621e7926ead93ee0e0df18
Simone <simone.giacomelli@gmail.com>
2023-01-02 16:08:08 +0000

    remove timeout

--- pynanto/pynanto/test/test_folder_conventions/test_conventions.py
+++ pynanto/pynanto/test/test_folder_conventions/test_conventions.py
@@ -13,4 +13,4 @@ def test_convention(page: Page, webserver_class):
     config = new_config(webserver_class)
 
     page.goto(config.webserver.localhost_url())
-    expect(page.locator('id=tag1')).to_have_value('Hello conventions!', timeout=60000)
+    expect(page.locator('id=tag1')).to_have_value('Hello conventions!')
