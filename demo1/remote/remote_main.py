a8a12a1add82b76a2f453410a703fafdee4ab07a
Simone Giacomelli <simone.giacomelli@lxc-xfce>
2023-01-11 20:02:32 +0100

    demo1

--- demo1/remote/remote_main.py
+++ demo1/remote/remote_main.py
@@ -3,23 +3,19 @@ import datetime
 from js import document
 
 from remote.execute_sql_widget import ExecuteSqlWidget
-from server.rpc import server_list_dir, server_execute_sql
 
 
 async def main():
-    directories = await server_list_dir('/')
-    document.body.innerHTML = f'ciao {datetime.datetime.now()}<br>{directories}'
+    # directories = await server_list_dir('/')
+    document.body.innerHTML = f'ciao {datetime.datetime.now()}<br><br><br>'
     btn = document.createElement('button')
     btn.innerHTML = 'clickme'
-    div = document.createElement('div')
-    div.innerHTML = 'loading'
 
     async def click(x):
-        result = await server_execute_sql('select * from rp_clienti')
-        div.innerHTML = str(result)
+        btn.onclick = None
+        ExecuteSqlWidget().append_to(document.body)
 
     btn.onclick = click
     document.body.append(btn)
-    document.body.append(div)
 
-    ExecuteSqlWidget().append_to(document.body)
+
