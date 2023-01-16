a5a08f63dc41416e9e15470816677a0128570300
Simone Giacomelli <simone.giacomelli@gmail.com>
2023-01-16 17:37:50 +0000

    bt exchange ok

--- pynanto_demo3/remote/bluetooth.py
+++ pynanto_demo3/remote/bluetooth.py
@@ -27,15 +27,61 @@ class BluetoothWidget(Widget):
             """
         )
 
-        self.btn1 = self
-
     async def btn1__click(self, event):
         # document.body.innerHTML = 'ciao2'
         from js import navigator
 
-        opt = to_js({'acceptAllDevices': True})
+        opt = to_js(
+            {
+                # 'acceptAllDevices': True,
+                'optionalServices': [0x1800, 0x1801, SERVICE],
+                'filters': [
+                    {'namePrefix': 'Car'}
+                ]
+            }
+        )
         device = await navigator.bluetooth.requestDevice(opt)
         console.log(device)
-        # devices = await navigator.bluetooth.getDevices()
-        # console.log(devices)
+        server = await device.gatt.connect()
 
+        print('server.getPrimaryService(SERVICE)...')
+        serviceSensors = await server.getPrimaryService(SERVICE)
+        self.serviceSensors = serviceSensors
+        print('getCharacteristic(READ_PACKET)...')
+        characteristic = await serviceSensors.getCharacteristic(READ_PACKET)
+        # print('getCharacteristic(WRITE_PACKET)...')
+        self.write_packet_char = await serviceSensors.getCharacteristic(WRITE_PACKET)
+        characteristic.addEventListener(
+            'characteristicvaluechanged',
+            create_proxy(self.valueChanged),
+            to_js({'capture': True})
+        )
+        print('startNotifications()...')
+        await characteristic.startNotifications()
+        print('startNotifications() done')
+
+    async def write_packet(self, packet_bytes):
+        await self.write_packet_char.writeValue(packet_bytes)
+
+    async def valueChanged(self, event):
+        raw = event.target.value
+        str = ''
+        for idx in range(raw.byteOffset, raw.byteLength):
+            ch = raw.getUint8(idx)
+            str += f'{ch:02x}'
+
+        cmd = chr(raw.getUint8(raw.byteOffset))
+        print('cmd', cmd, str)
+        log = print
+        if cmd == 'P':
+            log('answering with password')
+            from js import TextEncoder
+            pass_bytes = TextEncoder.new().encode('PD8GnDV5wkp8BeJ58PJ5')
+            await self.write_packet(pass_bytes)
+        if cmd == 'M':
+            log('answering with sending mode')
+            from js import Uint8Array
+            new = Uint8Array.new(bytes.fromhex('4D658E'))
+            console.log(new)
+            await self.write_packet(new)
+            # await self.write_packet(bytes([65, 120, 49 + 128, 16, 99, 101 + 128]))
