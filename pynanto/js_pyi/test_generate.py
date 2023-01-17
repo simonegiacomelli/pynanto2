5057c849220eeb88143ba3a5bb816f8eb33ea2ca
Simone <simone.giacomelli@gmail.com>
2023-01-17 18:07:47 +0000

    refactoring

--- pynanto/js_pyi/test_generate.py
+++ pynanto/js_pyi/test_generate.py
@@ -3,38 +3,31 @@ from js_pyi.generate import generate
 
 
 def test_simple():
-    idl = """
-/* https://dom.spec.whatwg.org/#interface-document */
-[Constructor]
+    actual = generate("""
 interface Document : Node {
-  [CEReactions, NewObject, Throws]
   FooElement createElement(DOMString localName);
 }    
-    """
-    actual = generate(idl)
-    assert len(actual) == 1
+    """)
 
-    g_arguments = GArguments(args=[GArg('localName', 'DOMString')])
-    g_function_def = GFunctionDef('createElement', arguments=g_arguments, returns='FooElement')
-    g_class_def = GClassDef('Document', bases=['Node'], body=[g_function_def])
-    actual0 = actual[0]
-    assert actual0 == g_class_def
+    assert actual == [GClassDef(
+        'Document', bases=['Node'], body=[
+            (GFunctionDef('createElement', arguments=(GArguments(args=[GArg('localName', 'DOMString')])),
+                          returns='FooElement'
+                          ))]
+    )]
 
 
 def test_optionals():
-    idl = """
+    actual = generate("""
 interface Document  {
   Element createElement(DOMString localName, optional (ElementCreationOptions or DOMString) options);
 }    
-    """
-    actual = generate(idl)
-    assert len(actual) == 1
-
-    g_arguments = GArguments(args=[
-        GArg('localName', 'DOMString'),
-        GArg('options', GUnion(['None', 'ElementCreationOptions', 'DOMString']))
-    ])
-    g_function_def = GFunctionDef('createElement', arguments=g_arguments, returns='Element')
-    g_class_def = GClassDef('Document', body=[g_function_def])
-    actual0 = actual[0]
-    assert actual0 == g_class_def
+    """)
+    assert actual == [GClassDef(
+        'Document', body=[
+            (GFunctionDef('createElement', arguments=(GArguments(args=[
+                GArg('localName', 'DOMString'),
+                GArg('options', GUnion(['None', 'ElementCreationOptions', 'DOMString']))
+            ])), returns='Element'))
+        ]
+    )]
