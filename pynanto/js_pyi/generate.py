ef3cc6167089c784d55d548cb4cfa9dfbbfa2f14
Simone <simone.giacomelli@gmail.com>
2023-01-18 11:24:10 +0000

    refactoring removing class

--- pynanto/js_pyi/generate.py
+++ pynanto/js_pyi/generate.py
@@ -7,161 +7,163 @@ from widlparser import Interface, InterfaceMember, Construct, TypeWithExtendedAt
 from js_pyi.g_dataclasses import *
 
 
-class Processor:
-    def __init__(self):
-        self.statements: List[GStmt] = []
+def g_symbol(symbol: Symbol):
+    expect_type(symbol, Symbol)
+    s = symbol.symbol
 
-    def parse(self, idl: str):
-        widl = widlparser.Parser()
-        widl.markup()
-        widl.parse(idl)
-        for c in widl.constructs:
-            try:
-                construct = self.g_construct(c)
-            except Exception as ex:
-                construct = GUnhandled(str(c), ex)
-            self.statements.append(construct)
-        return self.statements
+    return s
 
-    def c_interface(self, c: Interface):
-        members = remove_none(self.process_member(m) for m in c.members)
 
-        return GClassDef(c.name, bases=self.interface_bases(c), body=members)
+def g_default(default: Default):
+    expect_type(default, Default)
+    return default.value
 
-    def c_InterfaceMember(self, member: InterfaceMember):
-        if member.idl_type == 'method':
-            return self.c_InterfaceMember_method(member)
-        if member.idl_type == 'attribute':
-            return self.c_InterfaceMember_attribute(member)
-        todo(member.idl_type)
 
-    def c_InterfaceMember_attribute(self, member: InterfaceMember):
-        assert isinstance(member.member, Attribute)
-        assert isinstance(member.member.attribute, AttributeRest)
-        assert isinstance(member.member.attribute.type, TypeWithExtendedAttributes)
-        return GAttribute(
-            member.name,
-            self.g_type_with_extended_attributes(member.member.attribute.type)
-        )
+def g_type_identifier(ti: TypeIdentifier):
+    expect_type(ti, TypeIdentifier)
+    return ti.name
 
-    def c_InterfaceMember_method(self, member: InterfaceMember):
 
-        args = []
-        for a in member.arguments:
-            expect_type(a, Argument)
-            a: Argument
-            annotation = self.g_argument(a)
-            if not a.required:
-                annotation = GOptional(annotation)
-            g_arg = GArg(a.name, annotation)
-            if a.default is not None:
-                g_arg.default = self.g_default(a.default)
-            args.append(g_arg)
-        if isinstance(member.member, widlparser.Operation):
-            returns = str(member.member.return_type).strip()
-        else:
-            todo(member.member)
-        return GFunctionDef(member.name, GArguments(args=args), returns=returns)
+def g_primitive_type(primitive_type: PrimitiveType):
+    expect_type(primitive_type, PrimitiveType)
 
-    def process_member(self, member: Construct):
-        if isinstance(member, InterfaceMember):
-            return self.c_InterfaceMember(member)
-        elif isinstance(member, widlparser.ExtendedAttribute):
-            return None
-        else:
-            todo(member)
+    t = primitive_type.type
+    if isinstance(t, Symbol):
+        return g_symbol(t)
+    todo(t)
 
-    def g_construct(self, construct: Construct):
-        expect_type(construct, Construct)
 
-        if isinstance(construct, Interface):
-            return self.c_interface(construct)
+def g_non_any_type(non_any_type: NonAnyType):
+    expect_type(non_any_type, NonAnyType)
 
-        todo(construct)
+    t = non_any_type.type
+    if isinstance(t, PrimitiveType):
+        return g_primitive_type(t)
+    elif isinstance(t, TypeIdentifier):
+        return g_type_identifier(t)
+    elif isinstance(t, Symbol):
+        return g_symbol(t)
+    todo(t)
 
-    def g_argument(self, argument: Argument) -> str | GUnion:
-        expect_type(argument, Argument)
 
-        argument_type = argument.type
-        if isinstance(argument_type, TypeWithExtendedAttributes):
-            return self.g_type_with_extended_attributes(argument_type)
+def g_single_type(single_type: SingleType):
+    expect_type(single_type, SingleType)
+    t = single_type.type
+    if isinstance(t, AnyType):
+        todo(single_type)
+    elif isinstance(t, NonAnyType):
+        return g_non_any_type(t)
 
-        return str(argument_type.type)
+    return single_type.type_name
 
-    def g_type_with_extended_attributes(self, twea: TypeWithExtendedAttributes):
-        expect_type(twea, TypeWithExtendedAttributes)
 
-        t = twea.type
+def interface_bases(interface: Interface):
+    if interface.inheritance is None:
+        return []
+    return [str(interface.inheritance.base)]
 
-        if isinstance(t, UnionType):
-            return self.g_union_type(t)
-        elif isinstance(t, SingleType):
-            return self.g_single_type(t)
 
-        todo(t)
+def g_type_with_extended_attributes(twea: TypeWithExtendedAttributes):
+    expect_type(twea, TypeWithExtendedAttributes)
 
-    def g_union_type(self, union_type: UnionType):
-        ann = [str(a) for a in union_type.types]
-        # ann = ['None'] + ann
-        g_union = GUnion(ann)
-        return g_union
+    t = twea.type
 
-    def interface_bases(self, interface: Interface):
-        if interface.inheritance is None:
-            return []
-        return [str(interface.inheritance.base)]
+    if isinstance(t, UnionType):
+        return g_union_type(t)
+    elif isinstance(t, SingleType):
+        return g_single_type(t)
 
-    def g_single_type(self, single_type: SingleType):
-        expect_type(single_type, SingleType)
-        t = single_type.type
-        if isinstance(t, AnyType):
-            todo(single_type)
-        elif isinstance(t, NonAnyType):
-            return self.g_non_any_type(t)
+    todo(t)
 
-        return single_type.type_name
 
-    def g_non_any_type(self, non_any_type: NonAnyType):
-        expect_type(non_any_type, NonAnyType)
+def g_argument(argument: Argument) -> str | GUnion:
+    expect_type(argument, Argument)
 
-        t = non_any_type.type
-        if isinstance(t, PrimitiveType):
-            return self.g_primitive_type(t)
-        elif isinstance(t, TypeIdentifier):
-            return self.g_type_identifier(t)
-        elif isinstance(t, Symbol):
-            return self.g_symbol(t)
-        todo(t)
+    argument_type = argument.type
+    if isinstance(argument_type, TypeWithExtendedAttributes):
+        return g_type_with_extended_attributes(argument_type)
 
-    def g_primitive_type(self, primitive_type: PrimitiveType):
-        expect_type(primitive_type, PrimitiveType)
+    return str(argument_type.type)
 
-        t = primitive_type.type
-        if isinstance(t, Symbol):
-            return self.g_symbol(t)
-        todo(t)
 
-    def g_type_identifier(self, ti: TypeIdentifier):
-        expect_type(ti, TypeIdentifier)
-        return ti.name
+def c_InterfaceMember_method(member: InterfaceMember):
+    args = []
+    for a in member.arguments:
+        expect_type(a, Argument)
+        a: Argument
+        annotation = g_argument(a)
+        if not a.required:
+            annotation = GOptional(annotation)
+        g_arg = GArg(a.name, annotation)
+        if a.default is not None:
+            g_arg.default = g_default(a.default)
+        args.append(g_arg)
+    if isinstance(member.member, widlparser.Operation):
+        returns = str(member.member.return_type).strip()
+    else:
+        todo(member.member)
+    return GFunctionDef(member.name, GArguments(args=args), returns=returns)
 
-    def g_symbol(self, symbol: Symbol):
-        expect_type(symbol, Symbol)
-        s = symbol.symbol
-        # if s == 'boolean':
-        #     return 'bool'
 
-        return s
+def c_InterfaceMember_attribute(member: InterfaceMember):
+    assert isinstance(member.member, Attribute)
+    assert isinstance(member.member.attribute, AttributeRest)
+    assert isinstance(member.member.attribute.type, TypeWithExtendedAttributes)
+    return GAttribute(
+        member.name,
+        g_type_with_extended_attributes(member.member.attribute.type)
+    )
 
-    def g_default(self, default: Default):
-        expect_type(default, Default)
-        return default.value
+
+def c_InterfaceMember(member: InterfaceMember):
+    if member.idl_type == 'method':
+        return c_InterfaceMember_method(member)
+    if member.idl_type == 'attribute':
+        return c_InterfaceMember_attribute(member)
+    todo(member.idl_type)
+
+
+def process_member(member: Construct):
+    if isinstance(member, InterfaceMember):
+        return c_InterfaceMember(member)
+    elif isinstance(member, widlparser.ExtendedAttribute):
+        return None
+    else:
+        todo(member)
+
+
+def c_interface(c: Interface):
+    members = remove_none(process_member(m) for m in c.members)
+    return GClassDef(c.name, bases=interface_bases(c), body=members)
+
+
+def g_construct(construct: Construct):
+    expect_type(construct, Construct)
+
+    if isinstance(construct, Interface):
+        return c_interface(construct)
+
+    todo(construct)
+
+
+def g_union_type(union_type: UnionType):
+    ann = [str(a) for a in union_type.types]
+    g_union = GUnion(ann)
+    return g_union
 
 
 def generate(idl: str) -> List[GStmt]:
-    processor = Processor()
-    processor.parse(idl)
-    return processor.statements
+    statements: List[GStmt] = []
+    parser = widlparser.Parser()
+    parser.markup()
+    parser.parse(idl)
+    for c in parser.constructs:
+        try:
+            construct = g_construct(c)
+        except Exception as ex:
+            construct = GUnhandled(str(c), ex)
+        statements.append(construct)
+    return statements
 
 
 def remove_none(param):
--- pynanto/js_pyi/generate_js_pyi.py
+++ pynanto/js_pyi/generate_js_pyi.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 
-from js_pyi.generate import Processor
+from js_pyi.generate import generate
 
 
 def main():
-    processor = Processor()
-
     webidls_path = Path(__file__).parent / 'webidls'
+    txt = ''
     for f in webidls_path.glob('enabled/Document.webidl'):
         print(f'file {f}')
         txt = f.read_text()
-        processor.parse(txt)
+        break
 
-    print(processor.statements)
+    for stmt in generate(txt):
+        print(stmt)
 
 
 if __name__ == '__main__':
