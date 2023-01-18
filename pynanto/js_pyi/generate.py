a53fa0ebc797e7a28c7fa09f956e366d15f335bd
Simone <simone.giacomelli@gmail.com>
2023-01-18 14:49:24 +0000

    pyi string source generation

--- pynanto/js_pyi/generate.py
+++ pynanto/js_pyi/generate.py
@@ -10,5 +10,18 @@ def g_attribute(a: GAttribute) -> str:
     return a.name + ': ' + a.annotation
 
 
+_types_dict = {
+    'DOMString': 'str'
+}
+
+
+def _to_py_type(s: str) -> str:
+    return _types_dict.get(s, s)
+
+
 def g_method(m: GMethod) -> str:
-    return f'def {m.name}(): ...'
+    returns = ''
+    if m.returns != 'undefined':
+        returns = ' -> ' + _to_py_type(m.returns)
+
+    return f'def {m.name}(){returns}: ...'
--- pynanto/js_pyi/generate_test.py
+++ pynanto/js_pyi/generate_test.py
@@ -6,6 +6,11 @@ def test_attribute():
     assert actual == 'implementation: DOMImplementation'
 
 
-def test_method1():
+def test_method_1():
     actual = repr(GMethod('foo', arguments=(GArguments(args=[])), returns='undefined'))
     assert actual == 'def foo(): ...'
+
+
+def test_method_2():
+    actual = repr(GMethod('foo', arguments=(GArguments(args=[])), returns='DOMString'))
+    assert actual == 'def foo() -> str: ...'
