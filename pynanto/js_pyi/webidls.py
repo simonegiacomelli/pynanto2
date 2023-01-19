d97353ad939c4f569734dc236a561949e0d45837
Simone <simone.giacomelli@gmail.com>
2023-01-19 13:36:13 +0000

    implemented find_all

--- pynanto/js_pyi/webidls.py
+++ pynanto/js_pyi/webidls.py
@@ -1,13 +1,22 @@
 from pathlib import Path
+from typing import List
 
 
 def find(filename: str) -> Path:
+    for file in _find_all():
+        if file.name == filename:
+            return file
+    raise Exception(f'not found: `{file}`')
+
+
+def find_all() -> List[Path]:
+    return list(_find_all())
+
+
+def _find_all():
     root = Path(__file__).parent / 'webidls'
     folders = 'enabled unstable unavailable_option_primitive'.split(' ')
 
     for path in map(lambda s: root / s, folders):
-        files = path.glob('*.webidl')
-        for file in files:
-            if file.name == filename:
-                return file
-    raise Exception(f'not found: `{file}`')
+        files = list(path.glob('*.webidl'))
+        yield from files
--- pynanto/js_pyi/webidls_test.py
+++ pynanto/js_pyi/webidls_test.py
@@ -1,7 +1,14 @@
-from js_pyi.webidls import find
+from js_pyi.webidls import find, find_all
 
 
 def test_webidls_presence():
     assert 'interface Document' in find('Document.webidl').read_text()
     assert 'interface Bluetooth' in find('Bluetooth.webidl').read_text()
     assert 'interface PaymentRequest' in find('PaymentRequest.webidl').read_text()
+
+
+def test_findall():
+    actual = find_all()
+    assert find('Document.webidl') in actual
+    assert find('Bluetooth.webidl') in actual
+    assert find('PaymentRequest.webidl') in actual
