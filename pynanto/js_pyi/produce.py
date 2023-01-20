1d43751f826d604357a9c8c15dd24d54194ff5e9
Simone <simone.giacomelli@gmail.com>
2023-01-20 08:39:08 +0000

    refactoring

--- pynanto/js_pyi/produce.py
+++ pynanto/js_pyi/produce.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import ast
+import traceback
 from io import StringIO
 from pathlib import Path
 from typing import List
@@ -25,3 +27,21 @@ def produce(files: List[Path] | None = None) -> str:
         res.write(st.to_python() + '\n')
 
     return res.getvalue()
+
+
+def parse_product() -> bool:
+    exceptions = []
+    for file in find_all():
+        actual = produce([file])
+        try:
+            ast.parse(actual)
+        except Exception as ex:
+            exceptions.append((file, traceback.format_exc()))
+    print()
+    for file, ex in exceptions:
+        print('=' * 50)
+        print(file)
+        print(ex)
+
+    success = len(exceptions) == 0
+    return success
--- pynanto/js_pyi/produce_test.py
+++ pynanto/js_pyi/produce_test.py
@@ -1,8 +1,7 @@
 import ast
-import traceback
 
-from js_pyi.produce import produce
-from js_pyi.webidls import find, find_all
+from js_pyi.produce import produce, parse_product
+from js_pyi.webidls import find
 
 
 def test_correctness_0_quick():
@@ -11,17 +10,5 @@ def test_correctness_0_quick():
 
 
 def test_correctness_1_all():
-    exceptions = []
-    for file in find_all():
-        actual = produce([file])
-        try:
-            ast.parse(actual)
-        except Exception as ex:
-            exceptions.append((file, traceback.format_exc()))
-    print()
-    for file, ex in exceptions:
-        print('=' * 50)
-        print(file)
-        print(ex)
-
-    assert len(exceptions) == 0
+    success = parse_product()
+    assert success
