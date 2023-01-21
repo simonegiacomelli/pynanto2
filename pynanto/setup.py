7b73d3edaf64910a87b12663401f2fd2bd73d137
Simone <simone.giacomelli@gmail.com>
2023-01-21 06:41:30 +0000

    fix packaging

--- pynanto/setup.py
+++ pynanto/setup.py
@@ -1,14 +1,13 @@
-from pathlib import Path
-
 from setuptools import setup, find_packages
 
-install_requires = (Path(__file__).parent / 'requirements.txt').read_text().split('\n')
+# install_requires = (Path(__file__).parent / 'requirements.txt').read_text().split('\n')
 
 setup(
     name='pynanto',
-    version='0.1.0',
-    packages=find_packages(include=['pynanto', 'pynanto.*']),
-    install_requires=install_requires,
+    version='0.1.1',
+    packages=find_packages(include=['pynanto', 'pynanto.*', 'js', 'js.*']),
+    package_data={'js': ['__init__.pyi'], 'pynanto': ['bootstrap_pyodide.js']},
+    # install_requires=install_requires,
     classifiers=[
     ],
 )
