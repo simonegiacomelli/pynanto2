2f081134dde03fef372248409b325001130cf974
Simone <simone.giacomelli@gmail.com>
2023-03-02 15:17:33 +0000

    do not reuse ports

--- src/pynanto/server/find_port.py
+++ src/pynanto/server/find_port.py
@@ -1,17 +1,22 @@
 import errno
 import socket
 
+_start = 10000
+_end = 30000
 
-def find_port(start=10000, end=20000) -> int:
-    for candidate in range(start, end):
+
+def find_port() -> int:
+    global _start
+    while _start < _end:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                s.bind(('0.0.0.0', candidate))
-                return candidate
+                s.bind(('0.0.0.0', _start))
+                return _start
             except socket.error as e:
                 if e.errno != errno.EADDRINUSE:
                     raise
-    raise Exception(f'find_port(start={start},end={end}) no bindable tcp port found in interval')
+                _start += 1
+    raise Exception(f'find_port(start={_start},end={_end}) no bindable tcp port found in interval')
 
 
 if __name__ == '__main__':
