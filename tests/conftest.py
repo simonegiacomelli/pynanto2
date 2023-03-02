79782af53b3307d6c3e445a80030fbae209d0aa6
Simone <simone.giacomelli@gmail.com>
2023-03-02 19:08:00 +0000

    increase playwright timeout

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
+import inspect
+from types import FunctionType
+from typing import Callable, Any
+
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, PageAssertions, LocatorAssertions, APIResponseAssertions
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -9,3 +13,28 @@ def before_each_after_each(page: Page):
     page.on('pageerror', lambda exc: print(f'{sep}uncaught exception through pageerror: {sep}{exc}{sep}'))
     yield
     # print("I just experienced that this is not printed if the test fails")
+
+
+def patch_playwright_assertions() -> None:
+    # patch playwright assertion timeout to match our configuration
+    # this is temporary solution until playwright supports setting custom timeout for assertions
+    # github issue: https://github.com/microsoft/playwright-python/issues/1358
+
+    def patch_timeout(_member_obj: FunctionType) -> Callable:
+        def patch_timeout_inner(*args, **kwargs) -> Any:
+            parameters = inspect.signature(_member_obj).parameters
+            timeout_arg_index = list(parameters.keys()).index("timeout")
+            if timeout_arg_index >= 0:
+                if len(args) > timeout_arg_index:
+                    args = list(args)  # type: ignore
+                    args[timeout_arg_index] = 30000  # type: ignore
+                elif "timeout" in kwargs:
+                    kwargs["timeout"] = 30000
+            return _member_obj(*args, **kwargs)
+        return patch_timeout_inner
+
+    for assertion_cls in [PageAssertions, LocatorAssertions, APIResponseAssertions]:
+        for member_name, member_obj in inspect.getmembers(assertion_cls):
+            if isinstance(member_obj, FunctionType):
+                if "timeout" in inspect.signature(member_obj).parameters:
+                    setattr(assertion_cls, member_name, patch_timeout(member_obj))
