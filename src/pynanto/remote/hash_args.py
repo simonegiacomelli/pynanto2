8995db57fa25c2a8204ed44c45044fabd7363177
Simone <simone.giacomelli@gmail.com>
2023-03-10 17:59:19 +0000

    add hash_args

---
+++ src/pynanto/remote/hash_args.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+
+def hash_args(content: str | None = None):
+    result = dict()
+    if content is None:
+        from js import window
+        content = window.location.hash.removeprefix('#')
+    for entry in content.split('&'):
+        parts = entry.split('=', 1)
+        key = parts[0]
+        value = parts[1] if len(parts) > 1 else ''
+        result[key] = value
+    return result
+
+
+def test_hash_args():
+    target = hash_args('widget=W1')
+    assert target == {'widget': 'W1'}
+
+
+def test_hash_args_withNoValue():
+    assert hash_args('noval') == {'noval': ''}
+    assert hash_args('noval=') == {'noval': ''}
+
+
+def test_hash_args_multiple():
+    assert hash_args('a=1&b=2') == {'a': '1', 'b': '2'}
