aefdd9ae01bb7982827c34746ff6baf90eb35b79
Simone <simone.giacomelli@gmail.com>
2023-03-18 08:26:41 +0000

    log setup and crontab

---
+++ src/pynanto/common/log_setup.py
@@ -0,0 +1,41 @@
+import logging
+import os
+from dataclasses import dataclass
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
+
+
+def getLoggerFor(file):
+    return logging.getLogger(os.path.basename(file))
+
+
+logger = getLoggerFor(__file__)
+
+
+@dataclass(frozen=True)
+class LogConfig:
+    when: str = 'midnight'
+    interval: int = 1
+    backup_count: int = 90
+    format: str = "%(asctime)s [%(threadName)-12.12s] %(levelname)-5.5s %(name)s - %(message)s"
+
+
+default = LogConfig()
+
+
+def setup_logging(config: LogConfig = default):
+    log_folder = (Path(__file__).parent.parent / 'data/logs').absolute()
+    log_folder.mkdir(exist_ok=True, parents=True)
+    logging.basicConfig(
+        level=logging.INFO,
+        format=config.format,
+        handlers=[TimedRotatingFileHandler(
+            log_folder / "app-log",
+            when=config.when,
+            interval=config.interval,
+            backupCount=config.backup_count),
+            logging.StreamHandler()
+        ]
+    )
+    logger.info(f'Starting logging in folder {log_folder}')
+    logger.info(f'Working directory: {os.getcwd()}')
--- src/pynanto/common/test/crontab.py
+++ src/pynanto/common/test/crontab.py
@@ -3,9 +3,15 @@
 import time
 from datetime import datetime, timedelta
 
+from pynanto.common.log_setup import getLoggerFor
+
+logger = getLoggerFor(__file__)
+
 
 class AllMatch(set):
     def __contains__(self, item): return True  # match everything
+
+    def __str__(self) -> str: return '*'
 
 
 _allMatch = AllMatch()
@@ -24,7 +30,8 @@ def _to_set(obj):
 class Event(object):
     def __init__(self, action, sec=None, min=None, hour=None,
                  day=None, month=None, dow=None,
-                 args=(), kwargs={}):
+                 args=(), kwargs={}, log_action_run: bool = True):
+        self.log_action_run = log_action_run
         self.secs = _to_set(sec)
         self.mins = _to_set(min)
         self.hours = _to_set(hour)
@@ -46,6 +53,9 @@ class Event(object):
 
     def check(self, t: datetime):
         if self.matchtime(t):
+            if self.log_action_run:
+                string = ' '.join(map(str, [self.secs, self.mins, self.hours, self.days, self.months, self.dow]))
+                logger.info(f'Running job {string}')
             self.action(*self.args, **self.kwargs)
 
 
@@ -53,9 +63,9 @@ class CronTab(object):
     def __init__(self, *events):
         self.events = events
 
-    def run_thread(self):
+    def run_thread(self, name='Cron'):
         import threading
-        th = threading.Thread(target=self.run, daemon=True)
+        th = threading.Thread(target=self.run, daemon=True, name=name)
         th.start()
 
     def run(self):
